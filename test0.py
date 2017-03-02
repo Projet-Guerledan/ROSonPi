7358809b136a7fc1a65a99e27dd8bbcc06aef4a8
pierrebenet <pierrebenet96@gmail.com>
2017-03-02 12:22:12 +0100

    ajout d'un fichier de test basique qui a fonctionne sur la raspberry

---
+++ test0.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as gpio
+import time
+
+gpio.setmode(gpio.BOARD)
+gpio.setup(12,gpio.OUT)
+
+p1 = gpio.PWM(12,50)
+p1.start(5.0)
+time.sleep(4)
+p1.ChangeDutyCycle(10.)
+time.sleep(4)
+p1.stop()
+
