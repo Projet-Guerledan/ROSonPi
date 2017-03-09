cddb656d50780752ade6a7a399992fe4795cc530
MehdiN <nima.mehdi@ensta-bretagne.org>
2017-03-09 17:03:34 +0100

    dev

---
+++ glider_dir/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/master/Documents/Guerledan/ROSonPi/glider_dir/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/master/Documents/Guerledan/ROSonPi/glider_dir/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/master/Documents/Guerledan/ROSonPi/glider_dir/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/master/Documents/Guerledan/ROSonPi/glider_dir/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/master/Documents/Guerledan/ROSonPi/glider_dir/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/master/Documents/Guerledan/ROSonPi/glider_dir/src:$ROS_PACKAGE_PATH"
