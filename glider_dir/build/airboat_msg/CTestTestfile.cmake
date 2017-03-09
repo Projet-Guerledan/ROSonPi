77e677397952f87dc8332b1269bd8fe0b8552dc1
MehdiN <nima.mehdi@ensta-bretagne.org>
2017-03-09 16:58:22 +0100

    Fix

--- glider_dir/build/airboat_msg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/master/Documents/Guerledan/ROSonPi/glider_dir/src/airboat_msg
-# Build directory: /home/master/Documents/Guerledan/ROSonPi/glider_dir/build/airboat_msg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- glider_dir/build/cmd_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/master/Documents/Guerledan/ROSonPi/glider_dir/src/cmd_pkg
-# Build directory: /home/master/Documents/Guerledan/ROSonPi/glider_dir/build/cmd_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
